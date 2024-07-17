18d6bc9a1a955795449ff064333403732bc06ffa
JEET AHIRWAR <jeetahirwar664@gmail.com>
2024-07-18 00:47:06 +0530

    add some code backend in app.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://opportunityx.onrender.com/api/',
+    baseURL: 'https://opportunityx.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
