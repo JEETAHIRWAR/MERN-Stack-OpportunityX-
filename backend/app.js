3e88ec0a0a06c7450f39c63ba7c6e6d4f6b906db
JEET AHIRWAR <jeetahirwar664@gmail.com>
2024-07-17 23:08:52 +0530

    remove /api routes

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(cors({
 
 // Use routes
 app.use('/api/auth', authRoutes);
-app.use('/api', authRoutes);
+// app.use('/api', authRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/applications', applicationRoutes);
 
