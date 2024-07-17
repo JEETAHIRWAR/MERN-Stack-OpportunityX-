1769a54ca241fae2b4e1c2a6e5a9ad8a18e11159
JEET AHIRWAR <jeetahirwar664@gmail.com>
2024-07-17 22:48:19 +0530

    add default date func

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // Set the chunk size limit to 2000 KB
+  },
   server: {
     proxy: {
       '/api/jobs': 'http://localhost:3000',
