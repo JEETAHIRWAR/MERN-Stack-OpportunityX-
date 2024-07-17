aa72a7d329812a2438e38930cbc41dda56a2ec4a
JEET AHIRWAR <jeetahirwar664@gmail.com>
2024-07-18 00:53:31 +0530

    add some code backend in app.js

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(cors({
 
 // Use routes
 app.use('/api/auth', authRoutes);
-app.use('/api', jobRoutes);
+// app.use('/api', jobRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/applications', applicationRoutes);
 
